50cd8cbb3d62374357a1921e7692f52e17cfd682
Uwe Bessle <Uwe.Bessle@iteratec.de>
2015-03-18 23:08:53 +0100

    working REST interface call to determine beacon display name

--- cordova/beacon-scanner/www/js/util/beaconServer.js
+++ cordova/beacon-scanner/www/js/util/beaconServer.js
@@ -3,6 +3,8 @@
  */
 
 de_iteratec_beacon.beaconServer = (function() {
+    var util = de_iteratec_beacon.util;
+
     function getBeaconName(beacon, http) {
         var BASE_URL = 'http://iterabeacon.hh.iteratec.de:8080/beaconServer/api'
         var url = BASE_URL + '/beaconDevice?filter.uuid=' + beacon.uuid +
@@ -10,7 +12,11 @@ de_iteratec_beacon.beaconServer = (function() {
                                 '&filter.minor=' + beacon.minor;
         var restPromise = http.get(url)
         restPromise.success(function(restResponse) {
-            beacon.displayName = restResponse[0].displayName;
+            if (util.isDefined(restResponse.length) && (restResponse.length == 1) && (util.isDefined(restResponse[0].displayName))) {
+                beacon.displayName = restResponse[0].displayName;
+            } else {
+                beacon.displayName = 'unknown/'+beacon.major+'/'+beacon.minor;
+            }
         });
         restPromise.error(function() {
             console.log("error calling REST URL " + url);
